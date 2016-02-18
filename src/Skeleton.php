58b6d9faf2c6c4b4c420fd09a5a9c1ef04584816
john <john.smith@graze.com>
2016-02-18 16:07:27 +0000

    Year is no longer static

--- src/Skeleton.php
+++ src/Skeleton.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of graze/:package-name.
  *
- * Copyright (c) 2016 Nature Delivered Ltd. <https://www.graze.com>
+ * Copyright (c) :year Nature Delivered Ltd. <https://www.graze.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
