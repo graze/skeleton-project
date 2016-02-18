9b5120cc189bc605d8a3d0095455514367d205c3
john <john.smith@graze.com>
2016-02-18 16:09:09 +0000

    Add doc block to all PHP files

--- tests/src/ExampleTestCase.php
+++ tests/src/ExampleTestCase.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This file is part of graze/:package-name.
+ *
+ * Copyright (c) :year Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/package-name/blob/master/LICENSE.md
+ * @link https://github.com/graze/package-name
+ */
+
 namespace Graze\Skeleton\Test;
 
 class ExampleTestCase extends \PHPUnit_Framework_TestCase
